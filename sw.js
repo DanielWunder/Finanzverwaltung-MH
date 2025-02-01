066241acce2158965b527490fa4b583b3cf363d3
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2025-02-01 16:30:40 +0100

    Test SW

--- sw.js
+++ sw.js
@@ -13,3 +13,18 @@ async function onInstall(event) {
 async function onActivate(event) {
     console.info('Service worker: Activate');
 }
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "index.css",
+    "revision": "d3072ab3693c185313018e404e07d914"
+  },
+  {
+    "url": "index.html",
+    "revision": "a40871df1e3a91dd14b9e21363d4cc71"
+  },
+  {
+    "url": "index.js",
+    "revision": "6b73f45a2506a26e00e425688eaa6514"
+  }
+]);
