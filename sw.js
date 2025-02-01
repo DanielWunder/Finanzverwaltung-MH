30d57ed81d590e8a387c15890d2d6c7b126b2445
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2025-02-01 16:23:17 +0100

    Test new SW

--- sw.js
+++ sw.js
@@ -1,2 +1,15 @@
-if(!self.define){let e,i={};const s=(s,r)=>(s=new URL(s+".js",r).href,i[s]||new Promise((i=>{if("document"in self){const e=document.createElement("script");e.src=s,e.onload=i,document.head.appendChild(e)}else e=s,importScripts(s),i()})).then((()=>{let e=i[s];if(!e)throw new Error(`Module ${s} didn’t register its module`);return e})));self.define=(r,n)=>{const t=e||("document"in self?document.currentScript.src:"")||location.href;if(i[t])return;let o={};const c=e=>s(e,t),d={module:{uri:t},exports:o,require:c};i[t]=Promise.all(r.map((e=>d[e]||c(e)))).then((e=>(n(...e),o)))}}define(["./workbox-c9c1d482"],(function(e){"use strict";self.addEventListener("message",(e=>{e.data&&"SKIP_WAITING"===e.data.type&&self.skipWaiting()})),e.precacheAndRoute([{url:"categories.js",revision:"a7a788192193a2d611386a3c215607b6"},{url:"index.css",revision:"7d1182e69cb4ba1bdf326c5b85cc578b"},{url:"index.html",revision:"f254e643d5435291012b116554133e09"},{url:"index.js",revision:"b8eba9e8d8dac5d0b6e080fca5964726"},{url:"manifest.json",revision:"b94f50f24f3d1b7111608ff77e601945"}],{ignoreURLParametersMatching:[/^utm_/,/^fbclid$/]})}));
-//# sourceMappingURL=sw.js.map
+// service-worker.js (register this as a service worker)
+self.addEventListener('install', event => event.waitUntil(onInstall(event)));
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+self.addEventListener('fetch', () => { });
+
+async function onInstall(event) {
+    console.info('Service worker: Install');
+    // Here we are telling the service worker to activate right away when an update is pickedup.
+    // In our main JavaScript this will trigger 'controllerchange', which we can use to trigger an page reload.
+    self.skipWaiting();
+}
+
+async function onActivate(event) {
+    console.info('Service worker: Activate');
+}
