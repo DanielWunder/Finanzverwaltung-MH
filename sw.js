2567abc46ca0adab629a8321a354cd2c5437caba
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2025-02-01 16:37:08 +0100

    Revert all

--- sw.js
+++ sw.js
@@ -43,18 +43,3 @@ define(["./workbox-c9c1d482"], (function(e) {
     })
 }));
 //# sourceMappingURL=sw.js.map
-
-self.addEventListener('install', event => event.waitUntil(onInstall(event)));
-self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
-self.addEventListener('fetch', () => { });
-
-async function onInstall(event) {
-    console.info('Service worker: Install');
-    // Here we are telling the service worker to activate right away when an update is pickedup.
-    // In our main JavaScript this will trigger 'controllerchange', which we can use to trigger an page reload.
-    self.skipWaiting();
-}
-
-async function onActivate(event) {
-    console.info('Service worker: Activate');
-}
